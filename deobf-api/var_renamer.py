61936533d4224f4368b51ba7676311b50ef6313b
danethething <bathtub991@gmail.com>
2026-05-30 00:33:25 -0400

    Update var_renamer.py

--- deobf-api/var_renamer.py
+++ deobf-api/var_renamer.py
@@ -1,6 +1,6 @@
 import re
-import ast as py_ast
-from luaparser import ast, astnodes
+from luaparser import ast
+from luaparser.astnodes import Name, LocalAssign, Assign, Function, Fornum, Forin, While, Repeat, If, Call, Index, Return
 
 class VarRenamer:
     def __init__(self):
@@ -12,146 +12,94 @@ class VarRenamer:
             'print','require','pcall','xpcall','loadstring','load','pairs','ipairs',
             'setmetatable','getmetatable','rawset','rawget','tostring','tonumber',
             'table','string','math','coroutine','debug','io','os','unpack','select','type',
-            'assert','error','next','rawequal','_G','_ENV','self','arg'
+            'assert','error','next','rawequal','_G','_ENV'
         }
-        self.prefix = 'v'
-        self.name_format = 'alphabetic'
 
     def rename(self, source):
         try:
             tree = ast.parse(source)
+            self.var_map.clear()
+            self.counter = 0
+            self._collect_names(tree)
+            return self._replace_in_source(source)
         except:
             return source
 
-        self.var_map.clear()
-        self.counter = 0
-        self._collect_locals(tree)
-        self._walk_and_rename(tree)
-        return self._emit(tree)
+    def _collect_names(self, node):
+        if isinstance(node, Name):
+            name = node.id
+            if name not in self.reserved and not name.startswith('_G') and not name.startswith('_ENV'):
+                if name not in self.var_map:
+                    self.var_map[name] = f'v{self.counter}'
+                    self.counter += 1
+        if isinstance(node, (LocalAssign, Assign)):
+            for target in node.targets:
+                self._collect_names(target)
+            if hasattr(node, 'values'):
+                for val in node.values:
+                    self._collect_names(val)
+            return
+        if isinstance(node, Function):
+            if hasattr(node, 'name') and node.name:
+                self._collect_names(node.name)
+            if hasattr(node, 'args'):
+                for arg in node.args:
+                    if isinstance(arg, Name):
+                        self._collect_names(arg)
+            if hasattr(node, 'body'):
+                for stmt in node.body:
+                    self._collect_names(stmt)
+            return
+        if isinstance(node, Fornum):
+            self._collect_names(node.target)
+            self._collect_names(node.start)
+            self._collect_names(node.end)
+            if node.step:
+                self._collect_names(node.step)
+            for stmt in node.body:
+                self._collect_names(stmt)
+            return
+        if isinstance(node, Forin):
+            for target in node.targets:
+                self._collect_names(target)
+            for expr in node.iter:
+                self._collect_names(expr)
+            for stmt in node.body:
+                self._collect_names(stmt)
+            return
+        if isinstance(node, (While, Repeat)):
+            self._collect_names(node.condition)
+            for stmt in node.body:
+                self._collect_names(stmt)
+            return
+        if isinstance(node, If):
+            self._collect_names(node.test)
+            for stmt in node.body:
+                self._collect_names(stmt)
+            if hasattr(node, 'orelse') and node.orelse:
+                for stmt in node.orelse:
+                    self._collect_names(stmt)
+            return
+        if isinstance(node, Call):
+            self._collect_names(node.func)
+            for arg in node.args:
+                self._collect_names(arg)
+            return
+        if isinstance(node, Index):
+            self._collect_names(node.value)
+            self._collect_names(node.idx)
+            return
+        if isinstance(node, Return):
+            if hasattr(node, 'values'):
+                for val in node.values:
+                    self._collect_names(val)
+            return
+        if hasattr(node, 'children'):
+            for child in node.children():
+                self._collect_names(child)
 
-    def _generate_name(self):
-        if self.name_format == 'alphabetic':
-            name = ''
-            n = self.counter
-            while True:
-                name = chr(97 + (n % 26)) + name
-                n = n // 26
-                if n == 0:
-                    break
-            return self.prefix + '_' + name
-        return f'{self.prefix}{self.counter}'
-
-    def _should_rename(self, name):
-        if not name or not isinstance(name, str):
-            return False
-        if name in self.reserved:
-            return False
-        if name.startswith('_G.') or name.startswith('_ENV.'):
-            return False
-        if name.startswith('__') and name.endswith('__'):
-            return False
-        if len(name) == 1 and name.isalpha():
-            return False
-        return True
-
-    def _collect_locals(self, tree):
-        class LocalCollector(ast.ASTVisitor):
-            def __init__(self, renamer):
-                self.renamer = renamer
-                self.scopes = [set()]
-                self.function_params = set()
-
-            def visit_LocalFunction(self, node):
-                if node.name and self.renamer._should_rename(node.name):
-                    self.scopes[-1].add(node.name)
-                self.scopes.append(set())
-                self.generic_visit(node)
-                self.scopes.pop()
-
-            def visit_Function(self, node):
-                if node.name and self.renamer._should_rename(node.name):
-                    self.scopes[-1].add(node.name)
-                self.scopes.append(set())
-                for param in node.args:
-                    if param and self.renamer._should_rename(param):
-                        self.scopes[-1].add(param)
-                self.generic_visit(node)
-                self.scopes.pop()
-
-            def visit_LocalAssign(self, node):
-                for target in node.targets:
-                    if isinstance(target, astnodes.Name) and target.id:
-                        if self.renamer._should_rename(target.id):
-                            self.scopes[-1].add(target.id)
-                self.generic_visit(node)
-
-            def visit_For(self, node):
-                if node.var and isinstance(node.var, astnodes.Name) and node.var.id:
-                    if self.renamer._should_rename(node.var.id):
-                        self.scopes[-1].add(node.var.id)
-                self.scopes.append(set())
-                self.generic_visit(node)
-                self.scopes.pop()
-
-            def visit_Fornum(self, node):
-                if node.var and isinstance(node.var, astnodes.Name) and node.var.id:
-                    if self.renamer._should_rename(node.var.id):
-                        self.scopes[-1].add(node.var.id)
-                self.scopes.append(set())
-                self.generic_visit(node)
-                self.scopes.pop()
-
-            def visit_Foreach(self, node):
-                for var in node.vars:
-                    if isinstance(var, astnodes.Name) and var.id:
-                        if self.renamer._should_rename(var.id):
-                            self.scopes[-1].add(var.id)
-                self.scopes.append(set())
-                self.generic_visit(node)
-                self.scopes.pop()
-
-            def visit_Repeat(self, node):
-                self.scopes.append(set())
-                self.generic_visit(node)
-                self.scopes.pop()
-
-            def visit_While(self, node):
-                self.scopes.append(set())
-                self.generic_visit(node)
-                self.scopes.pop()
-
-            def visit_If(self, node):
-                self.scopes.append(set())
-                self.generic_visit(node)
-                self.scopes.pop()
-
-        collector = LocalCollector(self)
-        collector.visit(tree)
-
-    def _walk_and_rename(self, tree):
-        for node in ast.walk(tree):
-            if isinstance(node, astnodes.Name) and hasattr(node, 'id') and node.id:
-                if self._should_rename(node.id):
-                    if node.id not in self.var_map:
-                        name = self._generate_name()
-                        self.var_map[node.id] = name
-                        self.counter += 1
-
-    def _emit(self, tree):
-        source = ast.to_lua_source(tree)
-        for old, new in sorted(self.var_map.items(), key=lambda x: -len(x[0])):
-            source = re.sub(r'\b' + re.escape(old) + r'\b', new, source)
-        return source
-
-    def rename_only(self, source, mapping):
+    def _replace_in_source(self, source):
         result = source
-        for old, new in sorted(mapping.items(), key=lambda x: -len(x[0])):
+        for old, new in sorted(self.var_map.items(), key=lambda x: -len(x[0])):
             result = re.sub(r'\b' + re.escape(old) + r'\b', new, result)
         return result
-
-    def get_mapping(self):
-        return dict(self.var_map)
-
-    def reset(self):
-        self.var_map.clear()
-        self.counter = 0
