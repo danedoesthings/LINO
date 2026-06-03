6e9455853cffa244a9cb765475b022570035550f
danethething <bathtub991@gmail.com>
2026-06-03 15:52:58 -0400

    Create roblox.lua

---
+++ @25msrequireluvsu/roblox.lua
@@ -0,0 +1,5 @@
+return {
+ Vector3 = {new = function(x,y,z) return {X=x or 0, Y=y or 0, Z=z or 0} end},
+ CFrame = {new = function(x,y,z) return {X=x or 0, Y=y or 0, Z=z or 0} end},
+ Color3 = {new = function(r,g,b) return {R=r or 1, G=g or 1, B=b or 1} end},
+}
