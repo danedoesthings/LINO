fe68f996345861cf02e89aae21d92878dadb77df
danethething <bathtub991@gmail.com>
2026-06-04 06:14:40 -0400

    Update luaobfusctor.js

--- deobfuscators/luaobfusctor.js
+++ deobfuscators/luaobfusctor.js
@@ -1,23 +1,28 @@
-const { spawn } = require('child_process');
-const fs = require('fs');
-const path = require('path');
 const crypto = require('crypto');
 
-async function deobfuscateLuaObfuscator(code) {
+async function deobfuscate(code) {
     let result = code;
     
+    result = result.replace(/LuaObfuscator\.com[^\n]*\n/, '');
     result = result.replace(/local\s+_0x[a-f0-9]+\s*=\s*\{[^}]+\}/g, '');
-    result = result.replace(/local\s+function\s+_0x[a-f0-9]+\([^)]*\)[^{]*\{/g, 'function(');
     
     const stringDecryptPattern = /_0x([a-f0-9]+)\(["']([^"']+)["']\)/g;
-    result = result.replace(stringDecryptPattern, (_, id, str) => `"${str}"`);
+    result = result.replace(stringDecryptPattern, (_, id, str) => {
+        return `"${str}"`;
+    });
+    
+    const varPattern = /_0x[a-f0-9]+/g;
+    result = result.replace(varPattern, 'var');
     
     const xorPattern = /bit32\.bxor\((\d+),\s*(\d+)\)/g;
     result = result.replace(xorPattern, (_, a, b) => {
         return String(parseInt(a) ^ parseInt(b));
     });
     
+    result = result.replace(/local\s+_ENV\s*=\s*{[^}]+}/g, '');
+    result = result.replace(/getfenv\(\)\._ENV\s*=\s*getfenv\(\)/g, '');
+    
     return result;
 }
 
-module.exports = { deobfuscate: deobfuscateLuaObfuscator };
+module.exports = { deobfuscate };
